be3bbed354898b24a19ebcb0087f2854d3c76081
grzesiek <ggrekg@gmail.com>
2021-10-28 11:13:04 +0200

    add database postgresql

--- Django_sklep/settings.py
+++ Django_sklep/settings.py
@@ -77,11 +77,26 @@ WSGI_APPLICATION = 'Django_sklep.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sklep_django',
+        'USER': 'sklep_django',
+        'PASSWORD': 'sql',
+        'HOST': 'db',
+        'PORT': '5432',
+
     }
+
 }
 
 
